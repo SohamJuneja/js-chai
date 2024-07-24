cc67617133c0347e0a4006a23b80ed9a607e967f
SohamJuneja <138424822+SohamJuneja@users.noreply.github.com>
2024-07-24 21:43:53 +0000

    change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+ const accountId=144553
+ let accountEmail="junejasoham@gmail.com"
+ var accountPassword="12345"
+ accountCity="Jaipur"
+ let accountState;
+
+ //accountId=2
+ accountEmail="23bcs110@nith.ac.in"
+ accountPassword="54321"
+ accountCity="Alwar"
+
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
